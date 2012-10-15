58bcc81bc667f6d2b26428a5476b1aaabb36b04f
borgel <iborgel@gmail.com>
2012-10-15 02:11:43 -0700

    more work on guide data binding. It is still broken

--- iFixit7/Guide.xaml.cs
+++ iFixit7/Guide.xaml.cs
@@ -1,53 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Data.Linq.Mapping;
+﻿using System.Linq;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using System.Diagnostics;
-using System.Windows.Media.Imaging;
-using System.Windows.Data;
-using Microsoft.Phone;
-using System.IO;
 
 namespace iFixit7
 {
     public partial class GuideView : PhoneApplicationPage, INotifyPropertyChanged, INotifyPropertyChanging
     {
-        //add handlers so when we change this, it updates in the main view
-        private Guide _sourceGuide;
-        public Guide SourceGuide
+        /*
+         * The view model for a guide view
+         */
+        public class GuideViewModel
         {
-            get
+            //holds what goes in each column
+            public class ColContent
             {
-                return _sourceGuide;
-            }
-            set
-            {
-                if (_sourceGuide != value)
+                public string Title;
+                public string Image1, Image2, Image3;
+                public ObservableCollection<string> Lines { get; set; }
+
+                public ColContent(Step s)
                 {
-                    NotifyPropertyChanging("SourceGuide");
-                    _sourceGuide = value;
-                    NotifyPropertyChanged("SourceGuide");
+                    this.Title = s.StepIndex;
+                    this.Image1 = s.Image1;
+                    this.Image2 = s.Image2;
+                    this.Image3 = s.Image3;
+
+                    Lines = new ObservableCollection<string>();
+                    foreach (Lines l in s.Lines)
+                    {
+                        Lines.Add(l.Text);
+                    }
+                }
+            }
+
+            private Guide SourceGuide;
+
+            public ObservableCollection<ColContent> ColHeaders { get; set; }
+            public string Title = "asdasd";
+
+            /*
+             * Populate this VM with a pre-existing Guide
+             */
+            public GuideViewModel(Guide g)
+            {
+                SourceGuide = g;
+
+                ColHeaders = new ObservableCollection<ColContent>();
+
+                UpdateContentFromGuide(g);
+            }
+
+            /*
+             * Update the contents of this VM from the DB
+             */
+            public void UpdateContentFromGuide(Guide g)
+            {
+                foreach(Step s in g.Steps)
+                {
+                    ColHeaders.Add(new ColContent(s));
                 }
             }
         }
 
+
+        //add handlers so when we change this, it updates in the main view
+        private GuideViewModel _vm;
+        public GuideViewModel vm
+        {
+            get
+            {
+                return _vm;
+            }
+            set
+            {
+                if (_vm != value)
+                {
+                    NotifyPropertyChanging("vm");
+                    _vm = value;
+                    NotifyPropertyChanged("vm");
+                }
+            }
+        }
+
+
         string guideID = "";
+        private Guide SourceGuide;
 
         public GuideView()
         {
             InitializeComponent();
 
-            this.DataContext = SourceGuide;
+            this.DataContext = vm;
         }
 
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
@@ -66,6 +111,7 @@ namespace iFixit7
                 App.mDB.GuidesTable.DeleteOnSubmit(SourceGuide);
                 App.mDB.SubmitChanges();
             }
+            vm = new GuideViewModel(SourceGuide);
 
             //api call. The callback will be fired to populate the view
             new JSONInterface2().populateGuideView(this.guideID, insertGuideIntoDB);
@@ -79,10 +125,11 @@ namespace iFixit7
             App.mDB.SubmitChanges();
 
             //force view model to update
-            this.DataContext = SourceGuide;
+            vm.UpdateContentFromGuide(SourceGuide);
+            this.DataContext = vm;
 
             //hide the loading bar
-            LoadingBarInfo.Visibility = System.Windows.Visibility.Collapsed;
+            //LoadingBarInfo.Visibility = System.Windows.Visibility.Collapsed;
             return true;
         }
 
